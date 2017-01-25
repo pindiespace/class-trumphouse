088d889bc2f9c966da869610f13cb7eda8960869
System Administrator <root@AI251LAB0218M01.local>
2017-01-25 14:39:57 -0800

    defined base es5 objects

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# trumphouse
-principles of es5 object oriented scripting
+Trumphouse
+======================
+Gameplay
+
+1. At start, load all the characters onto the screen (arena). 
+
+2. Trump is on the left. A paddle is near Trump. If the paddle moves, it collides with Trump and makes him move.
+
+3. The other Presidential candidates for a vertical wall halfway across the screen.
+
+4. The right-hand side of the screen shows an image of the White House.
+
+5. When Trump collides with other candidates, they are destroyed, and he bounces back. He also bounces off the walls of the arena.
+
+6. When Trump finds a hole in the wall of candidates, he goes to the right hand side. When he is over the White House, it becomes the Trump House and the game is over.
+=======================
+
+
+Features
+1.	Load characters and make them visible onscreen
+2.	Provide a way for the user to affect the position of a paddle (or some such)
+3.	Define how characters move onscreen
+4.	Define how characters react when they collide with each other
+5.	Define a scoring method
--- public/js/trumphouse.js
+++ public/js/trumphouse.js
@@ -1,13 +1,81 @@
 /** 
  * trumphouse - an es5 OOP program
  */
+
+// VIEW-LIKE
+// makes a visual object that you can see onscreen with a DIV
+var DOMPiece = function ( image ) {
+    this.elem = document.createElement( 'div' );
+    this.elem.appendChild( document.createElement( 'img') );
+};
+
+// GAME CHARACTERS
+var Trump = function ( name, src ) {
+	this.elem = new DOMPiece( src );
+	this.fullName = name; 
+};
+
+var Candidate = function ( name, src ) {
+	this.elem = new DOMPiece( src );
+	this.fullName = name; 
+};
+
+var User = function ( level, src ) {
+	this.elem = new DOMPiece( src );
+	this.skill = level;
+};
+
+
+// THE MAIN GAME
 var trumphouse = (function() {
+
+	this.BEGINNER = 1;
+	this.EXPERIENCED = 2;
 
 	function init () {
 
 		console.log( 'in init' );
 
+		this.candidates = []; // array holding candidates
+
+		// attach a Trump inside 'trumphouse'
+		this.trump = new Trump( "Donald J. Trump", 'img/players/trump.png' );
+
+		// attach the player
+		this.player = new User(this.BEGINNER, 'PUT AN IMAGE HERE');
+
+		// attach candidates to the candidates array
+		this.candidates.push( new Candidate( "Hillary Clinton", 'img/bricks/clinton.png') );
+		this.candidates.push( new Candidate( "Ted Cruz" ) );
+		this.candidates.push( new Candidate( "Bernie Sanders" ) );
+		this.candidates.push( new Candidate( "John Kasich" ) );
+		this.candidates.push( new Candidate( "Jill Stein" ) );
+		this.candidates.push( new Candidate( "Gary Johnson" ) );
+
 	};
+
+	/* 
+	 * MODEL
+	 * Store the game score
+	 * Score timing (if the game is time)
+	 * Store parameters for game size, object size defaults
+	 */
+
+
+	/* 
+	 * VIEW
+	 * The background to the game
+	 * The White House image
+	 * A "Trump" Logo
+	 * Characters (candidates and Trump)
+     * The user (a paddle)
+	 */
+
+	/*
+	 * CONTROLLER
+	 * Detect user pressing the arrow keys
+	 * Detect collisions 
+     */
 
 
 	return {
